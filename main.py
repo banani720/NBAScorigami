cac75121a2cfddd5058e40a657382307aa935da0
Basel Anani <baselanani@u.boisestate.edu>
2021-02-12 13:02:51 -0800

    Got all the team id's and game list for 02-03 szn

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tweepy as twpy
 import pandas as pd
 from nba_api.stats.endpoints import teamgamelog as games
+from nba_api.stats.static import teams
 import json
-import numpy as np
+
 # Set up keys and secrets for twitter API
 api_consumer_key = "OJdv9UD3PdE6PYujpnKtdZHx2"
 api_secret = "wpgAhU2fXqmOpYm4uO8bbVCQGjJlRNX2g4MMSc4LVjv7OU4NQV"
@@ -15,20 +16,11 @@ auth.set_access_token(access_token, access_secret)
 # Auth the twitter API
 api = twpy.API(auth)
 
-hawks_data = games.TeamGameLog(team_id=1610612737, season="2002-03").get_json()
-hawks_json = json.loads(hawks_data)
-
-df = pd.json_normalize(hawks_json)
-
-result_set_array = df["resultSets"].to_numpy()
-
-for i in result_set_array:
-    for j in i:
-        results_json = json.dumps(j, indent=2)
-print(results_json)
-
-
-
-
-
-
+all_teams = teams.get_teams()
+teams = pd.DataFrame(all_teams)
+gamelog_array = []
+for id in teams.id:
+    for i in range(2002, 2003):
+        game_list = games.TeamGameLog(team_id=id, season=i).get_json()
+        gamelog_array.append(game_list)
+print(gamelog_array[0])
